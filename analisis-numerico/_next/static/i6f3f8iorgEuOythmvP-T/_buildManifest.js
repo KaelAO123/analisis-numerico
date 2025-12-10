55c6749724976e88ed17a19bedd3f82bb824a652
Kae <caereyes0@gmail.com>
2025-12-10 10:26:52 -0400

    Pagina funcionando, error arreglado

---
+++ analisis-numerico/_next/static/i6f3f8iorgEuOythmvP-T/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/analisis-numerico//_next/:path+",
+        "destination": "/analisis-numerico/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
